0794644ceef62287b9285da47a62b5c14dd23059
kukechen <chen.huan@infoloop.cn>
2021-08-13 19:48:42 +0800

    ~

---
+++ src/main/java/com/kukechen/keepgoing/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.kukechen.keepgoing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
