461cf1b316ea29d184bde69d353de4181a4afc36
kukechen <chen.huan@infoloop.cn>
2021-09-01 13:33:09 +0800

    ~

--- src/main/java/com/kukechen/keepgoing/controller/DirectiveController.java
+++ src/main/java/com/kukechen/keepgoing/controller/DirectiveController.java
@@ -1,24 +1,16 @@
 package com.kukechen.keepgoing.controller;
 
 import com.kukechen.keepgoing.entity.UserInfo;
-import com.kukechen.keepgoing.mapper.UserInfoMapper;
 import com.kukechen.keepgoing.utils.JsonUtil;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
-
-    @Autowired
-    private UserInfoMapper userInfoMapper;
+public class DirectiveController {
 
     @GetMapping("/")
     public String index() {
         try {
-            UserInfo user = userInfoMapper.selectByPrimaryKey(1);
-            return JsonUtil.getJsonString(user);
         } catch (Exception e) {
             e.printStackTrace();
         }
